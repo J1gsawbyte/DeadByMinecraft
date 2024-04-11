229073d982ae9f67183fbea9daf6e7a444215ded
J1gsaw <j1gsaw@qq.com>
2024-04-11 12:19:05 +0800

    添加KillerAttackEvent和对应的Listener 完善KillerAttackEvent

---
+++ src/main/java/com/j1gsaw/deadbyminecraft/Killer/Events/KillerAttackEvent.java
@@ -0,0 +1,26 @@
+package com.j1gsaw.deadbyminecraft.Killer.Events;
+
+import com.j1gsaw.deadbyminecraft.Killer.Killer;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class KillerAttackEvent extends Event {
+
+    private boolean isOnSurvivor;
+
+    private Killer killer;
+
+    public KillerAttackEvent(Killer killer, boolean isOnSurvivor) {
+        this.killer = killer;
+        this.isOnSurvivor = isOnSurvivor;
+    }
+
+    private static final HandlerList handlers = new HandlerList();
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+}
---
+++ src/main/java/com/j1gsaw/deadbyminecraft/Killer/Listener/Main/KillerAttackListener.java
@@ -0,0 +1,7 @@
+package com.j1gsaw.deadbyminecraft.Killer.Listener.Main;
+
+import org.bukkit.event.Listener;
+
+public class KillerAttackListener implements Listener {
+
+}
